da0fbc98adc0df0e6ea24959db707ab7c633cec8
MathewAviles <juanaviles12345@gmail.com>
2024-03-13 01:20:31 -0500

    Mi decimo commit

--- Tienda/__init__.py
+++ Tienda/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
+from flask_migrate import Migrate
 import os
 
 # create the app
@@ -12,6 +13,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URL")
 #postgres://dbmatcomerce_user:VlSbUtyyJNJ7NhM097eDZWC0XVhc6y6A@dpg-cnoh6cg21fec73dvdu00-a.oregon-postgres.render.com/dbmatcomerce
 # initialize the app with the extension
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 # encriptación del sistema de logeo
 app.config['SECRET_KEY']='MATcomerce159753%&ñ'
 bcrypt = Bcrypt(app)
